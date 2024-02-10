32138bc9ca71c48b9e915e2be4d8a44ff7c14dac
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2024-02-10 13:03:08 +0100

    day 12 skipped
    welp

---
+++ 2023/day12.py
@@ -0,0 +1,13 @@
+from utils import read_input
+
+
+def part1(content: str) -> int:
+    return None
+
+
+def part2(content: str) -> int:
+    return None
+
+
+print(f"Part 1: {part1(read_input(__file__))}")  # TBD
+print(f"Part 2: {part2(read_input(__file__))}")  # TBD
